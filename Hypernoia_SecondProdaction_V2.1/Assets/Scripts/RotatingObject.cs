8179589d5fe5f2f798da1d9a527658e1d4c79ec7
useruser <agatamagata91@gmail.com>
2024-01-16 19:21:12 +0100

    Rotation Script and Antispeach room

---
+++ Hypernoia_SecondProdaction_V2.1/Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour
+{
+    public float rotationSpeed = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+}
