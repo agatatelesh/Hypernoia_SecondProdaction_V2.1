c3417334f67dfaae78496eb662b0b84d36369068
useruser <agatamagata91@gmail.com>
2024-01-16 15:52:08 +0100

    Skeleton in a Mars Location

--- Hypernoia_SecondProdaction_V2.1/Assets/Scripts/SetActiveScript.cs
+++ Hypernoia_SecondProdaction_V2.1/Assets/Scripts/SetActiveScript.cs
@@ -4,22 +4,47 @@ using UnityEngine;
 
 public class SetActiveScript : MonoBehaviour
 {
-    public GameObject toActivate;
-    public GameObject toDeactivate;
+    public List<GameObject> toBeActive_firstGroup = new List<GameObject>();
+     public List<GameObject> toBeActive_secondGroup = new List<GameObject>();
+
+    public bool isActive_1 = false;
+    public bool isActive_2 = false;
+
+   // public GameObject toActivate;
+    //public GameObject toDeactivate;
 
     // Update is called once per frame
      void Update()
     {
         if(Input.GetKeyDown(KeyCode.L)){
-            ActivateObj();
+            ActivateFirstGroup();
+            
+        }
+        if(Input.GetKeyDown(KeyCode.K)){
+            ActivateSecondGroup();
             
         }
         
     }
 
-    public void ActivateObj(){
-        toActivate.SetActive(true);
-        toDeactivate.SetActive(false);
+    public void ActivateFirstGroup(){
+        //toActivate.SetActive(true);
+        //toDeactivate.SetActive(false);
+        isActive_1 = !isActive_1;
+        foreach(GameObject obj in toBeActive_firstGroup){
+            obj.SetActive(isActive_1);
+        }
     }
+
+    public void ActivateSecondGroup(){
+        //toActivate.SetActive(true);
+        //toDeactivate.SetActive(false);
+        isActive_2 = !isActive_2;
+        foreach(GameObject obj in toBeActive_secondGroup){
+            obj.SetActive(isActive_2);
+        }
+    }
+
+
 
 }
